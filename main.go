89ea8f0fa1ccce83e9094dcbf3cfd47f09cf1d88
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-07-24 00:51:44 +0900

    HTMLテンプレートを実装

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"sync"
+	"html/template"
+	"path/filepath"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	temp1    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.temp1 = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.temp1.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServer:", err)
+	}
+}
