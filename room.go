d9be398e80beae7e8c117395eab7e986e7b546a5
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-07-24 02:55:09 +0900

    チャットルームをHTTPハンドラ化

--- room.go
+++ room.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"github.com/gorilla/websocket"
+	"net/http"
+	"log"
+)
+
 type room struct {
 	forward chan []byte
 	join    chan *client
@@ -27,4 +33,28 @@ func (r *room) run() {
 			}
 		}
 	}
+}
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
 }
