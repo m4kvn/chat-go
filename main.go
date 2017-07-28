26a0243a6bc14670f15043f7e09f2e53bc03684b
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-07-29 06:16:53 +0900

    外部アカウントのログイン実装

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ import (
 	"flag"
 	"chat-go/trace"
 	"os"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 type templateHandler struct {
@@ -24,18 +28,40 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.temp1.Execute(w, r)
 }
 
+const (
+	facebookClientId = "566880443702209"
+	githubClientId   = "e966db44e197cb80fee7"
+	googleClientId   = "613930549056-ma8v8hqdmd6hfbb4kq3diql6ca2tk5kp.apps.googleusercontent.com"
+)
+
 func main() {
 	addr := flag.String("addr", ":8080", "アプリケーションのアドレス")
+	securityKey := flag.String("key", "", "セキュリティキー")
+	facebookClientSecret := flag.String("facebook", "", "Facebookクライアントシークレット")
+	githubClientSecret := flag.String("github", "", "GitHubクライアントシークレット")
+	googleClientSecret := flag.String("google", "", "Googleクラインアントシークレット")
 	flag.Parse() // フラグを解釈
+
+	gomniauth.SetSecurityKey(*securityKey)
+	gomniauth.WithProviders(
+		facebook.New(facebookClientId, *facebookClientSecret, "http://localhost:8080/auth/callback/facebook"),
+		github.New(githubClientId, *githubClientSecret, "http://localhost:8080/auth/callback/facebook"),
+		google.New(googleClientId, *googleClientSecret, "http://localhost:8080/auth/callback/facebook"),
+	)
+
+	// チャットルームを作成
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
-	//http.Handle("/", &templateHandler{filename: "chat.html"})
+
+	// http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
+
 	// チャットルームを開始
 	go r.run()
+
 	// Webサーバを起動
 	log.Println("Webサーバを開始 ポート：", *addr)
 	if err := http.ListenAndServe(*addr, nil); err != nil {
