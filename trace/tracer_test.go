4882f163354d6d7747f31c1318708f68fb45d91d
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-07-29 01:40:41 +0900

    Create trace/tracer_test

---
+++ trace/tracer_test.go
@@ -0,0 +1,19 @@
+package trace
+
+import (
+	"testing"
+	"bytes"
+)
+
+func TestNew(t *testing.T) {
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("Newからの戻り値がnilです")
+	} else {
+		tracer.Trace("Hello, trace package")
+		if buf.String() != "Hello, trace package\n" {
+			t.Errorf("'%s'という誤った文字列が出力されました", buf.String())
+		}
+	}
+}
