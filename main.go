c927e169e592e121193654ee06d10d1044716b92
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-07-29 04:50:19 +0900

    サインイン用のページを作成

--- main.go
+++ main.go
@@ -31,6 +31,7 @@ func main() {
 	r.tracer = trace.New(os.Stdout)
 	//http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.Handle("/room", r)
 	// チャットルームを開始
 	go r.run()
